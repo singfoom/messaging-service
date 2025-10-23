a5e80cc1ba57761540eadde43e9e4a387cc4ef8e
singfoom <singfoom@gmail.com>
2025-10-23 18:02:19 -0500

    Adding participant length test to conversation test

--- lib/messaging_service/messages/conversation.ex
+++ lib/messaging_service/messages/conversation.ex
@@ -1,11 +1,20 @@
 defmodule MessagingService.Messages.Conversation do
   use Ecto.Schema
+  import Ecto.Changeset
 
   alias MessagingService.Messages.Message
 
   schema "conversations" do
-    field :attachments, {:array, :string}
+    field :participants, {:array, :string}
     has_many :messages, Message
     timestamps(type: :utc_datetime)
+  end
+
+  @doc false
+  def changeset(conversation, attrs) do
+    conversation
+    |> cast(attrs, [:participants])
+    |> validate_required([:participants])
+    |> validate_length(:participants, is: 2)
   end
 end
